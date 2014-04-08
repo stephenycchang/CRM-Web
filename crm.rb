ede6f35ed2f0c18b5ad73ccbb410477ce49c4184
Stephen Chang <stephenycchang@gmail.com>
2014-04-07 23:25:53 -0400

    Changed to Datamapper

--- crm.rb
+++ crm.rb
@@ -1,92 +1,118 @@
 require 'sinatra'
-require './contacts.rb'
 require './rolodex.rb'
-# require 'data_mapper'
+require 'data_mapper'
 
-# DataMapper.setup(:default, "sqlite3:database.sqlite3")
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
 
-@@rolodex = Rolodex.new
+class Contact
+  include DataMapper::Resource
 
-@@rolodex.create_contact(1001, "Matt", "Holtom", "matt@gmail.com", "ksjdflksajdf")
-@@rolodex.create_contact(1002, "Stephen", "Chang", "stephen@gmail.com", "ksjdflksajdf")
+  property :id, Serial
+  property :first_name, String
+  property :last_name, String
+  property :email, String
+  property :note, String
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+#   @@counter = 1000
+
+#   attr_accessor :first_name, :last_name, :email, :note, :id
+
+#   def initialize(counter, first_name, last_name, email, note)
+#     @first_name = first_name
+#     @last_name = last_name
+#     @email = email
+#     @note = note
+#     @@counter += 1
+#     @id = @@counter
+#   end
+
+#   def firstname
+#     @first_name
+#   end
+
+#   def lastname
+#     @last_name
+#   end
+
+#   def e_mail
+#     @email
+#   end
+
+#   def paper
+#     @note
+#   end
+
+#   def number
+#     @id
+#   end
+
+#   def to_s
+#     " ID: #{id} -#{first_name} #{last_name} -  #{email} ---Notes: #{note}"
+#   end
+# end
+
+
+# @@rolodex = Rolodex.new
+
+# @@rolodex.create_contact(1001, "Matt", "Holtom", "matt@gmail.com", "ksjdflksajdf")
+# @@rolodex.create_contact(1002, "Stephen", "Chang", "stephen@gmail.com", "ksjdflksajdf")
 
 get "/" do
   erb :index  
 end
 
-# get "/delete" do
-#   @contacts = @@rolodex.show_contacts
-#   erb :delete
-# end
-
-# get "/new" do
-#   erb :add_contact
-# end
-
-# get "/contacts" do
-#   @contacts = @@rolodex.contacts
-#   erb :contacts
-# end
-
-# get "/modify" do
-#   @contacts = @@rolodex.show_contacts
-#   erb :modify
-# end
-
-# post "/new" do
-#   erb :add_contact
-# end 
-
-# post "/delete" do
-#   @@rolodex.delete_contact(params[:id])
-#   redirect to ("/contacts")
-# end
-
-# post "/contacts" do
-#      # new_contact = Contact.new(params[:id],params[:first_name], params[:last_name], params[:email], params[:note])
-#      #  @@rolodex.create_contact_alternate(new_contact)
-#   @@rolodex.create_contact(params[:counter], params[:first_name], params[:last_name], params[:email], params[:note])
-#   # contacts = Contact.new(params[:id], params[:first_name], params[:last_name], params[:email], params[:note])
-#   redirect to ("/contacts")
-# end
-
-# post "/modify" do
-#   @@rolodex.modify_contact(params[:id], params[:new_first_name], params[:new_last_name], params[:new_email], params[:new_note])
-#   redirect to ("/contacts")
-# end
-
-get '/contacts' do  #1.  read all contacts
-  @contacts = @@rolodex.contacts
+#1.  read all contacts
+get '/contacts' do  
+  # @contacts = @@rolodex.contacts
+  @contacts = Contact.all
   erb :contacts
 end
 
-post '/contacts'  do  #2.   Create a contact
-  @@rolodex.create_contact(params[:counter], params[:first_name],params[:last_name], params[:email], params[:notes])
+#2.   Create a contact
+post '/contacts'  do  
+  # @@rolodex.create_contact(params[:counter], params[:first_name],params[:last_name], params[:email], params[:notes])
+  contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :note => params[:note]
+    )
   redirect to ("/contacts")
 end
 
-get '/contacts/new' do        #7.   Return a form for a new contact
+#7.   Return a form for a new contact
+get '/contacts/new' do        
   erb :add_contact
 end
 
-get '/contacts/:id' do #3.   Read one contact with that id
-  @@rolodex.find(params[:id])
-  erb :show_contacts
+#3.   Read one contact with that id
+get '/contacts/:id' do 
+  @contact = Contact.get(params[:id].to_i)
+  if @contact
+    erb :show_contacts
+  else
+    raise Sinatra::NotFound
+  end
 end
 
-put '/contacts/:id' do  #4.   Update the existing contact with that id
+#4.   Update the existing contact with that id
+put '/contacts/:id' do  
   @@rolodex.modify_contact(params[:id], params[:new_first_name], params[:new_last_name], params[:new_email], params[:new_note])
   redirect to :contacts
 end
 
-delete '/contacts/:id' do  #5.   Delete the existing contact with that id
+#5.   Delete the existing contact with that id
+delete '/contacts/:id' do  
   @contacts = @@rolodex.delete_contact(params[:id])
   redirect to :contacts
 end
 
-get '/contacts/:id/edit' do  #6.   Return a form for updating an existing contact
+#6.   Return a form for updating an existing contact
+get '/contacts/:id/edit' do  
   @contact = @@rolodex.find(params[:id])
   erb :modify
 end
-
-
